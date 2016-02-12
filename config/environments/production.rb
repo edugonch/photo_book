76d1ea00937273f8cb0af2a8d04179e9690c3914
Eduardo Gonzalez <edugonch@gmail.com>
2016-02-12 19:57:01 +0000

    Add logs to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  #Logs for heroku
+  config.logger = Logger.new(STDOUT) 
+  config.log_level = :info
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.paperclip_defaults = {
