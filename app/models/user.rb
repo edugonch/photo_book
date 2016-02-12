99ebf9499e707b1d127b7c44b64e1c7f91b069bb
Eduardo Gonzalez <edugonch@gmail.com>
2016-02-12 22:10:28 +0000

    Change regect on nested attributes for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
   has_many :images, dependent: :destroy
 
   accepts_nested_attributes_for :images,
-                                reject_if: proc { |attributes| attributes['lat'].blank? && attributes['lng'].blank? && attributes['file'].blank? },
+                                reject_if: proc { |attributes| attributes['lat'].blank? || attributes['lng'].blank? || attributes['file'].blank? },
                                 allow_destroy: true
 end
