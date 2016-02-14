9afb152b1be376172fa51ec1f71d58c4724fb39a
Eduardo Gonzalez <edugonch@gmail.com>
2016-02-13 21:29:15 -0600

    Add wilpaginate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def profile
     user = User.find(params[:id])
-    @images = user.images
+    @images = user.images.paginate(:page => params[:page], :per_page => 10)
   end
 
 end
